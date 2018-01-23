7f969deb59f05650a969940a4ffd441b19e47513
Steven Polanco <spolanco2694@gmail.com>
2018-01-23 22:01:35 +0000

    Associate user model with comment model

---
+++ v8/routes/campgrounds.js
@@ -0,0 +1,58 @@
+var express         = require("express");
+var Campground      = require("../models/campground");
+var router          = express.Router();
+
+//Index - Show all Campgrounds
+router.get("/", function(req, res) {
+    //Get all campgrounds from db
+    Campground.find({}, function(err, allcampgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds/index", { campgrounds: allcampgrounds, currentUser: req.user });
+        }
+    }); 
+
+});
+
+//Create - Add new Campgrounds to DB
+router.post("/", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc };
+
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - Display form to create a new Campground
+router.get("/new", function(req, res) {
+    res.render("campgrounds/new");
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res) {
+    //find campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(foundCampground);
+            //render show template 
+            res.render("campgrounds/show", { campground: foundCampground });
+        }
+    });
+});
+
+module.exports = router;
